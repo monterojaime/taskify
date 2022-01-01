b56743690e4a94e01c88f0adc8a78563504925cc
Jaime Montero <monterocjaime@gmail.com>
2022-01-01 18:33:01 -0500

    Read values from form when click on add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 let buttonEl = document.querySelector("#save-task");
 let taskList = document.querySelector("#task-to-do");
 let taskForm = document.querySelector("#task-form");
+let taskNameInput = document.querySelector("input[name='task-name']");
+let taskTypeInput = document.querySelector("select[name='task-type']");
 
 let createTaskHandler = function (event) {
-    
+    console.log(taskNameInput);
     event.preventDefault();
 
     let newTask = document.createElement("li");
     newTask.classList.add("task-item");
-    newTask.textContent = "Brand New Added Task";
+
+    let taskInfo = document.createElement("div");
+
+    taskInfo.classList.add("task-info");
+    taskInfo.innerHTML = "<h3 class='task-name'>"+taskNameInput.value+"</h3><span class='task-type'>"+taskTypeInput.value+"</span>";
+    newTask.appendChild(taskInfo);
     taskList.appendChild(newTask);
+    //taskNameInput.value = "";
 }
 
 taskForm.addEventListener('submit', createTaskHandler);
