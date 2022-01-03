be080e40c075454990b20176ecf87872e8769547
Jaime Montero <monterocjaime@gmail.com>
2022-01-02 21:30:11 -0500

    Add Delete Update and Status Dropdown dynamically to each task - add 2 new columns to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let buttonEl = document.querySelector("#save-task");
 let taskList = document.querySelector("#task-to-do");
 let taskForm = document.querySelector("#task-form");
+let taskIdCounter = 0;
+
 
 
 let taskFormHandler = function (event) {
@@ -29,13 +31,56 @@ let taskFormHandler = function (event) {
 let createTaskEl = function(dataObj){
     let newTask = document.createElement("li");
     newTask.classList.add("task-item");
+    newTask.setAttribute("data-task-id", taskIdCounter);
 
     let taskInfo = document.createElement("div");
 
     taskInfo.classList.add("task-info");
     taskInfo.innerHTML = "<h3 class='task-name'>"+dataObj.name+"</h3><span class='task-type'>"+dataObj.type+"</span>";
     newTask.appendChild(taskInfo);
+
+    let taskActions = createTaskActions(taskIdCounter);
+    newTask.appendChild(taskActions);
+
     taskList.appendChild(newTask);
-}
+    
+    taskIdCounter++;
+};
+
+let createTaskActions = function(taskId){
+    let actionContainer = document.createElement('div');
+    actionContainer.classList.add('task-actions');
+
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.classList.add("btn","edit-btn");
+    editBtn.setAttribute("data-task-id", taskId);
+    actionContainer.appendChild(editBtn);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("btn","delete-btn");
+    deleteBtn.setAttribute("data-task-id", taskId);
+    actionContainer.appendChild(deleteBtn);
+
+    let statusSelect = document.createElement("select");
+    statusSelect.classList.add("select-status");
+    statusSelect.setAttribute("name", "status-change");
+    statusSelect.setAttribute("data-task-id", taskId);
+
+    actionContainer.appendChild(statusSelect);
+
+    let statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (let i = 0; i < statusChoices.length; i ++){
+        let statusOption = document.createElement("option");
+        statusOption.textContent = statusChoices[i];
+        statusOption.setAttribute("value", statusChoices[i]);
+
+        statusSelect.appendChild(statusOption);
+    }
+
+    return actionContainer;
+};
 
 taskForm.addEventListener('submit', taskFormHandler);
